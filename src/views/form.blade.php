8ae7965f83f7282d0febb35b35547664dcb6e9d2
Armand Grundmanis <agrundmanis@inbox.lv>
2018-10-03 23:45:39 +0300

    Removed container

--- src/views/form.blade.php
+++ src/views/form.blade.php
@@ -3,90 +3,85 @@
 @include('laracms.dashboard::partials.summernote')
 
 @section('content')
+    <form enctype="multipart/form-data" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col-md-12">
 
-    <div class="content">
-        <div class="container-fluid">
-            <form enctype="multipart/form-data" method="POST">
-                @csrf
-                <div class="row">
-                    <div class="col-md-12">
-
-                        <div class="form-group row">
-                            <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('form.icon') }}</label>
-                            <div class="col-md-6">
-                                <input class="form-control" id="icon" type="file" name="icon">
-                            </div>
-                            @if(isset($template))
-                                <img width="300" src="{{ asset($template->icon) }}" alt="">
-                            @endif
-                        </div>
+                <div class="form-group row">
+                    <label for="image" class="col-md-4 col-form-label text-md-right">{{ __('form.icon') }}</label>
+                    <div class="col-md-6">
+                        <input class="form-control" id="icon" type="file" name="icon">
+                    </div>
+                    @if(isset($template))
+                        <img width="300" src="{{ asset($template->icon) }}" alt="">
+                    @endif
+                </div>
 
 
-                        <!-- Nav tabs -->
-                        <div class="form-group">
-                            <ul class="nav nav-tabs" role="tablist">
-                                @foreach($locales as $key => $locale)
-                                    <li role="presentation" @if(!$key) class="active" @endif>
-                                        <a href="#title_{{ $locale }}" data-toggle="tab">
-                                            {{ $locale }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        <!-- Tab panes -->
-                        <div class="tab-content">
-                            <label title="value">Title</label>
+                <!-- Nav tabs -->
+                <div class="form-group">
+                    <ul class="nav nav-tabs" role="tablist">
+                        @foreach($locales as $key => $locale)
+                            <li role="presentation" @if(!$key) class="active" @endif>
+                                <a href="#title_{{ $locale }}" data-toggle="tab">
+                                    {{ $locale }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <label title="value">Title</label>
 
-                            @foreach($locales as $key => $locale)
-                                <div class="tab-pane @if(!$key) active @endif" id="title_{{ $locale }}">
-                                    <div class="form-group">
-                                    <textarea class="summernote" name="{{ $locale }}[title]">
-                                        {{ formValue($template ?? null, 'title', $locale) }}
-                                    </textarea>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
-
-
-                        <!-- Nav tabs -->
-                        <div class="form-group">
-                            <ul class="nav nav-tabs" role="tablist">
-                                @foreach($locales as $key => $locale)
-                                    <li role="presentation" @if(!$key) class="active" @endif>
-                                        <a href="#{{ $locale }}" data-toggle="tab">
-                                            {{ $locale }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        <!-- Tab panes -->
-                        <div class="tab-content">
-                            <label title="value">Value</label>
-
-                            @foreach($locales as $key => $locale)
-                                <div class="tab-pane @if(!$key) active @endif" id="{{ $locale }}">
-                                    <div class="form-group">
-                                        <textarea class="summernote" name="{{ $locale }}[body]">
-                                            {{ formValue($template ?? null, 'body', $locale) }}
-                                        </textarea>
-                                    </div>
-                                </div>
-                            @endforeach
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('menu.save') }}
-                                </button>
+                    @foreach($locales as $key => $locale)
+                        <div class="tab-pane @if(!$key) active @endif" id="title_{{ $locale }}">
+                            <div class="form-group">
+                            <textarea class="summernote" name="{{ $locale }}[title]">
+                                {{ formValue($template ?? null, 'title', $locale) }}
+                            </textarea>
                             </div>
                         </div>
+                    @endforeach
+                </div>
+
+
+                <!-- Nav tabs -->
+                <div class="form-group">
+                    <ul class="nav nav-tabs" role="tablist">
+                        @foreach($locales as $key => $locale)
+                            <li role="presentation" @if(!$key) class="active" @endif>
+                                <a href="#{{ $locale }}" data-toggle="tab">
+                                    {{ $locale }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+                <!-- Tab panes -->
+                <div class="tab-content">
+                    <label title="value">Value</label>
+
+                    @foreach($locales as $key => $locale)
+                        <div class="tab-pane @if(!$key) active @endif" id="{{ $locale }}">
+                            <div class="form-group">
+                                <textarea class="summernote" name="{{ $locale }}[body]">
+                                    {{ formValue($template ?? null, 'body', $locale) }}
+                                </textarea>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+
+                <div class="form-group row mb-0">
+                    <div class="col-md-6 offset-md-4">
+                        <button type="submit" class="btn btn-primary">
+                            {{ __('menu.save') }}
+                        </button>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
-    </div>
+    </form>
 @endsection
--- src/views/index.blade.php
+++ src/views/index.blade.php
@@ -1,44 +1,38 @@
 @extends('laracms.dashboard::layouts.app', ['page' => 'Mail Templates'])
 
 @section('content')
+    <div class="form-group">
+        <a class="btn btn-success" href="{{ route('laracms.mail-template.create') }}">Create</a>
+    </div>
 
-    <div class="content">
-        <div class="container-fluid">
-
-            <div class="form-group">
-                <a class="btn btn-success" href="{{ route('laracms.mail-template.create') }}">Create</a>
-            </div>
-
-            <div class="table-responsive">
-                <table class="table table-striped">
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Icon</th>
-                            <th>Title</th>
-                            <th>Created at</th>
-                            <th></th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($templates as $template)
-                        <tr>
-                            <td>{{ $template->id }}</td>
-                            <td><img width="100" src="{{ asset($template->icon) }}" alt=""></td>
-                            <td>{{ $template->title }}</td>
-                            <td>{{ $template->created_at }}</td>
-                            <td>
-                                <a href="{{ route('laracms.mail-template.edit', $template->id) }}">Edit</a>
-                                |
-                                <a onclick="return confirm('Are you sure?')"
-                                   href="{{ route('laracms.mail-template.destroy', $template->id) }}">Delete</a>
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
-                {{ $templates->links() }}
-            </div>
-        </div>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>#</th>
+                    <th>Icon</th>
+                    <th>Title</th>
+                    <th>Created at</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+            @foreach($templates as $template)
+                <tr>
+                    <td>{{ $template->id }}</td>
+                    <td><img width="100" src="{{ asset($template->icon) }}" alt=""></td>
+                    <td>{{ $template->title }}</td>
+                    <td>{{ $template->created_at }}</td>
+                    <td>
+                        <a href="{{ route('laracms.mail-template.edit', $template->id) }}">Edit</a>
+                        |
+                        <a onclick="return confirm('Are you sure?')"
+                           href="{{ route('laracms.mail-template.destroy', $template->id) }}">Delete</a>
+                    </td>
+                </tr>
+            @endforeach
+            </tbody>
+        </table>
+        {{ $templates->links() }}
     </div>
 @endsection
